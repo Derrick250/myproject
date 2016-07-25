d5429297444badd23fa88bd46fd58ec74409dc49
DERRICK <derrick@sendy.co.ke>
2016-07-25 17:10:33 +0300

    Add cardviews

--- app/src/main/java/geeks/freaky/contraceptive_sos/MainActivity.java
+++ app/src/main/java/geeks/freaky/contraceptive_sos/MainActivity.java
@@ -1,39 +1,44 @@
+
 package geeks.freaky.contraceptive_sos;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.widget.CardView;
+import android.view.View;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements View.OnClickListener{
 
+    CardView cardView;
+    CardView cardView1;
+    CardView cardView2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        cardView = (CardView)findViewById(R.id.card_view);
+        cardView1 = (CardView)findViewById(R.id.card_view1);
+        cardView2 = (CardView)findViewById(R.id.card_view2);
+
+        cardView.setOnClickListener(this);
+        cardView1.setOnClickListener(this);
+        cardView2.setOnClickListener(this);
+
     }
 
-
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.card_view:
+                System.out.println(">>>>>>>>>>>>>> Clicked 1");
+                break;
+            case R.id.card_view1:
+                System.out.println(">>>>>>>>>>>>>> Clicked 2");
+                break;
+            case R.id.card_view2:
+                System.out.println(">>>>>>>>>>>>>> Clicked 3");
+                break;
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
         }
-
-        return super.onOptionsItemSelected(item);
     }
 }
