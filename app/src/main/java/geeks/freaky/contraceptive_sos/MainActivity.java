fdd9d46f99916054ad160169001aa3a8aab7858e
theTypan <kelvinmuchiri63@gmail.com>
2016-07-28 16:22:54 +0300

    second commit

--- app/src/main/java/geeks/freaky/contraceptive_sos/MainActivity.java
+++ app/src/main/java/geeks/freaky/contraceptive_sos/MainActivity.java
@@ -1,26 +1,34 @@
 
 package geeks.freaky.contraceptive_sos;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 
 public class MainActivity extends ActionBarActivity implements View.OnClickListener{
 
-    CardView cardView;
+    CardView cardview;
     CardView cardView1;
     CardView cardView2;
+    Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        cardView = (CardView)findViewById(R.id.card_view);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        cardview = (CardView)findViewById(R.id.card_view);
         cardView1 = (CardView)findViewById(R.id.card_view1);
         cardView2 = (CardView)findViewById(R.id.card_view2);
 
-        cardView.setOnClickListener(this);
+        cardview.setOnClickListener(this);
         cardView1.setOnClickListener(this);
         cardView2.setOnClickListener(this);
 
@@ -31,6 +39,8 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
         switch (view.getId()){
             case R.id.card_view:
                 System.out.println(">>>>>>>>>>>>>> Clicked 1");
+                Intent i = new Intent(this,Condom.class);
+                startActivity(i);
                 break;
             case R.id.card_view1:
                 System.out.println(">>>>>>>>>>>>>> Clicked 2");
